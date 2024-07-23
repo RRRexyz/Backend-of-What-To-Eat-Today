437074a1edbf1f0b513f47425a3d66735b256f98
徐昊 <2840722773@qq.com>
2024-07-23 23:48:37 +0800

    7.23:连接数据库，实现了查看食堂当前饭菜列表和增加饭菜

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
+from sql_app import crud, models, schemas
+from sql_app.database import SessionLocal, engine
+
+from typing import List
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+@app.get("/dish/{canteen}")
+def get_current_canteen_dishes(canteen: schemas.Canteen, db: Session = Depends(get_db)):
+    if canteen == schemas.Canteen.xinyuan:
+        return crud.get_canteen_all_dishes(db, "欣园")
+    elif canteen == schemas.Canteen.yueyuan:
+        return crud.get_canteen_all_dishes(db, "悦园")
+
+
+@app.post("/dishadd")
+def add_dishes(dishes: List[schemas.DishItem], db: Session = Depends(get_db)):
+    crud.add_dishes(db, dishes)
+
+
+
+# @app.post("/users/", response_model=schemas.User)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="Email already registered")
+#     return crud.create_user(db=db, user=user)
+
+
+# @app.get("/users/", response_model=list[schemas.User])
+# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     users = crud.get_users(db, skip=skip, limit=limit)
+#     return users
+
+
+# @app.get("/users/{user_id}", response_model=schemas.User)
+# def read_user(user_id: int, db: Session = Depends(get_db)):
+#     db_user = crud.get_user(db, user_id=user_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+
+
+# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
+# def create_item_for_user(
+#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+# ):
+#     return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+# @app.get("/items/", response_model=list[schemas.Item])
+# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     items = crud.get_items(db, skip=skip, limit=limit)
+#     return items
---
+++ sql_app/schemas.py
@@ -0,0 +1,67 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import List
+from dataclasses import dataclass
+
+
+class Canteen(str, Enum):
+    xinyuan = "欣园"
+    yueyuan = "悦园"
+
+
+@dataclass
+class DishItem(BaseModel):
+    """DishItem"""
+    """餐厅名称"""
+    canteen: str
+    """楼层号"""
+    floor: int
+    """饭菜编号"""
+    id: int
+    """份量"""
+    measure: str
+    """名称"""
+    name: str
+    """价格"""
+    price: float
+    """窗口号"""
+    window: int
+
+    
+class DishList(BaseModel):
+    dishlist: List[DishItem]
+
+
+
+# class ItemBase(BaseModel):
+#     title: str
+#     description: str | None = None
+
+
+# class ItemCreate(ItemBase):
+#     pass
+
+
+# class Item(ItemBase):
+#     id: int
+#     owner_id: int
+
+#     class Config:
+#         orm_mode = True
+
+
+# class UserBase(BaseModel):
+#     email: str
+
+
+# class UserCreate(UserBase):
+#     password: str
+
+
+# class User(UserBase):
+#     id: int
+#     is_active: bool
+#     items: list[Item] = []
+
+#     class Config:
+#         orm_mode = True
